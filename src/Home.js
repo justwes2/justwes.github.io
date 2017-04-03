1c7e1a999435a15f6ba7d89de4083321f3441fc0
Wes <wes.coffay@gmail.com>
2017-04-03 15:46:14 -0400

    fixed missed links

--- src/Home.js
+++ src/Home.js
@@ -29,8 +29,8 @@ class Home extends Component {
               <h1>Wes Coffay</h1>
               <h3>Apprentice Web Developer</h3>
               <nav>
-                <div className='navOp'><Link to="/">Home </Link></div>
-                <div className='navOp'><Link to="/contact">Contact </Link></div>
+                <div className='navOp'><Link to="/portfolio_page/">Home </Link></div>
+                <div className='navOp'><Link to="/portfolio_page/contact">Contact </Link></div>
               </nav>
             </div>
           </div>
